bac823cc742aeeb7e882a9e2db56926b63dcf50a
Takashi Kanemoto <ttskch@gmail.com>
2020-05-02 19:06:49 +0900

    docs: ✏️ update example

--- docs/assets/js/8.428841d3.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{484:function(n,e,t){"use strict";t.r(e);var l=t(27),s=Object(l.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);e.default=s.exports}}]);
