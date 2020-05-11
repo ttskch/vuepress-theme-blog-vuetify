0633ab4ceac126768c44f15318ca99c906a8afc0
Takashi Kanemoto <ttskch@gmail.com>
2020-05-12 00:40:11 +0900

    docs: ✏️ update example

---
+++ docs/assets/js/19.0a0ea14a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{379:function(n,w,o){}}]);
---
+++ docs/assets/js/8.a371cb12.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{430:function(n,t,e){"use strict";e.r(t);var o=e(2),u=Object(o.a)({},(function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("BaseLayout",{scopedSlots:n._u([{key:"content",fn:function(){return[e("h1",[n._v("404 Not Found")])]},proxy:!0}])})}),[],!1,null,null,null);t.default=u.exports}}]);
