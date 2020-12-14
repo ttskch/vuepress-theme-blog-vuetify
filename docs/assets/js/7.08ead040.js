2cd4c0fc2b23a1f669b183fe92ec017c46309376
Takashi Kanemoto <ttskch@gmail.com>
2020-12-14 21:10:16 +0900

    docs: ✏️ update example

--- docs/assets/js/7.08ead040.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{490:function(n,t,e){"use strict";e.r(t);var o=e(4),u=Object(o.a)({},(function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("BaseLayout",{scopedSlots:n._u([{key:"content",fn:function(){return[e("h1",[n._v("404 Not Found")])]},proxy:!0}])})}),[],!1,null,null,null);t.default=u.exports}}]);
